711e436a67c916d5cf1a2ea8696b6789ddbcbf6a
sileshi haile <sileshih@gmail.com>
2021-05-14 18:37:12 +0100

    one class added

---
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -0,0 +1,22 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Sileshi Haile Woldemariame
+ * Date: 2021-05-14
+ * Time: 12:45
+ * Project:springdocker
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+
+
+    public int add(int a,int b){
+        return 0;
+    }
+    public int multiply(int a,int b){
+        return 0;
+    }
+    public int divide(int a,int b){
+        return 0;
+    }
+}
